0be1abd5e32d5a0f84413f6c108ad51e44119896
tylergubser <tylergubser2200@gmail.com>
2022-08-02 09:52:06 -0500

    update

---
+++ game-website/src/Canvas.js
@@ -0,0 +1,10 @@
+import React, { useRef } from 'react'
+
+const Canvas = props => {
+  
+  const canvasRef = useRef(null)
+  
+  return <canvas ref={canvasRef} {...props}/>
+}
+
+export default Canvas
